de83bb6c90d166a2f4499869211c84d5a1fea4fc
Ng Guoyou <fauxface+github@gmail.com>
2014-06-13 21:52:51 +0800

    Cleaner materials for main

--- main.rs
+++ main.rs
@@ -31,24 +31,20 @@ fn main() {
     let mut lights: Vec<Box<Light>> = Vec::with_capacity(max_lights);
     lights.push(box PointLight {position: Vec3 {x: 50.0, y: 90.0, z: 50.0}, color: Vec3 {x: 1.0, y: 1.0, z: 1.0}});
 
-    let grey1 = box FlatMaterial {color: Vec3 {x: 0.6, y: 0.6, z: 0.6}};
-    let grey2 = box FlatMaterial {color: Vec3 {x: 0.6, y: 0.6, z: 0.6}};
-    let grey3 = box FlatMaterial {color: Vec3 {x: 0.6, y: 0.6, z: 0.6}};
-    let red   = box FlatMaterial {color: Vec3 {x: 1.0, y: 0.0, z: 0.0}};
-    let green = box FlatMaterial {color: Vec3 {x: 0.0, y: 1.0, z: 0.0}};
-
-    let blue1   = box FlatMaterial {color: Vec3 {x: 0.0, y: 0.0, z: 1.0}};
-    let blue2   = box FlatMaterial {color: Vec3 {x: 0.3, y: 0.3, z: 1.0}};
+    let grey  = FlatMaterial {color: Vec3 {x: 0.6, y: 0.6, z: 0.6}};
+    let red   = FlatMaterial {color: Vec3 {x: 1.0, y: 0.0, z: 0.0}};
+    let green = FlatMaterial {color: Vec3 {x: 0.0, y: 1.0, z: 0.0}};
+    let blue  = FlatMaterial {color: Vec3 {x: 0.0, y: 0.0, z: 1.0}};
 
     let mut prims: Vec<Box<Prim>> = Vec::with_capacity(max_prims);
-    prims.push(box Plane {a: 0.0,  b:  0.0, c: 1.0, d: 0.0,   material: grey1 }); // Ahead
-    prims.push(box Plane {a: 0.0,  b: -1.0, c: 0.0, d: 100.0, material: grey2 }); // Bottom
-    prims.push(box Plane {a: 0.0,  b:  1.0, c: 0.0, d: 0.0,   material: grey3 }); // Top
-    prims.push(box Plane {a: 1.0,  b:  0.0, c: 0.0, d: 0.0,   material: red   }); // Left
-    prims.push(box Plane {a: -1.0, b:  0.0, c: 0.0, d: 100.0, material: green }); // Right
+    prims.push(box Plane {a: 0.0,  b:  0.0, c: 1.0, d: 0.0,   material: box grey }); // Ahead
+    prims.push(box Plane {a: 0.0,  b: -1.0, c: 0.0, d: 100.0, material: box grey }); // Bottom
+    prims.push(box Plane {a: 0.0,  b:  1.0, c: 0.0, d: 0.0,   material: box grey }); // Top
+    prims.push(box Plane {a: 1.0,  b:  0.0, c: 0.0, d: 0.0,   material: box red   }); // Left
+    prims.push(box Plane {a: -1.0, b:  0.0, c: 0.0, d: 100.0, material: box green }); // Right
 
-    prims.push(box Sphere {center: Vec3 {x: 30.0, y: 15.0, z: 20.0}, radius: 15.0, material: blue1});
-    prims.push(box Sphere {center: Vec3 {x: 70.0, y: 17.0, z: 80.0}, radius: 17.0, material: blue2});
+    prims.push(box Sphere {center: Vec3 {x: 30.0, y: 15.0, z: 20.0}, radius: 15.0, material: box blue});
+    prims.push(box Sphere {center: Vec3 {x: 70.0, y: 17.0, z: 80.0}, radius: 17.0, material: box blue});
 
     let camera = camera::Camera::new(
         Vec3 {x: 50.0, y: 25.0, z: 300.0},
