b1db2670bb4d9e35fb45293afa59c09e672b0979
Ng Guoyou <fauxface+github@gmail.com>
2014-08-07 18:58:38 +0800

    Add test to Ray

--- src/raytracer/ray.rs
+++ src/raytracer/ray.rs
@@ -3,6 +3,17 @@ use raytracer::Intersection;
 use scene::Scene;
 use vec3::Vec3;
 
+#[cfg(test)]
+use geometry::prim::Prim;
+#[cfg(test)]
+use geometry::prims::Sphere;
+#[cfg(test)]
+use light::light::Light;
+#[cfg(test)]
+use material::materials::FlatMaterial;
+#[cfg(test)]
+use raytracer::VecPrimContainer;
+
 pub struct Ray {
     pub origin: Vec3,
     pub direction: Vec3,
@@ -40,11 +51,59 @@ impl Ray {
                     } else {
                         nearest_hit
                     }
-                }
-                None => {nearest_hit}
+                },
+                None => nearest_hit
             };
         }
 
         nearest_hit
     }
+}
+
+#[test]
+fn it_gets_the_nearest_hit() {
+    let mut lights: Vec<Box<Light+Send+Share>> = Vec::new();
+
+    let mut prims: Vec<Box<Prim+Send+Share>> = Vec::new();
+    let mat = FlatMaterial { color: Vec3::one() };
+    let sphere_top = Sphere {
+        center: Vec3::zero(),
+        radius: 1.0,
+        material: box mat.clone()
+    };
+    let sphere_mid = Sphere {
+        center: Vec3 { x: -1.0, y: 0.0, z: 0.0 },
+        radius: 1.0,
+        material: box mat.clone()
+    };
+    let sphere_bot = Sphere {
+        center: Vec3 { x: -2.0, y: 0.0, z: 0.0 },
+        radius: 1.0,
+        material: box mat.clone()
+    };
+    prims.push(box sphere_top);
+    prims.push(box sphere_mid);
+    prims.push(box sphere_bot);
+
+    let scene = Scene {
+        lights: lights,
+        background: Vec3::one(),
+        prim_strat: box VecPrimContainer::new(prims),
+        skybox: None
+    };
+
+    let intersecting_ray = Ray::new(
+        Vec3 { x: 10.0, y: 0.0, z: 0.0 },
+        Vec3 { x: -1.0, y: 0.0, z: 0.0 }
+    );
+
+    let intersection = intersecting_ray.get_nearest_hit(&scene);
+    assert_eq!(1.0, intersection.unwrap().position.x);
+
+    let non_intersecting_ray = Ray::new(
+        Vec3 { x: 10.0, y: 0.0, z: 0.0 },
+        Vec3 { x: 1.0, y: 0.0, z: 0.0 });
+
+    let non_intersection = non_intersecting_ray.get_nearest_hit(&scene);
+    assert!(non_intersection.is_none());
 }
